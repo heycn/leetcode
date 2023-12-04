c0714f66bad94daeae701971641de9c58df729ad
heycn <heycn@foxmail.com>
2023-12-04 22:35:14 +0800

    Refactor compose function in FunctionComposition.js

--- javascript/FunctionComposition.js
+++ javascript/FunctionComposition.js
@@ -2,11 +2,14 @@
  * @param {Function[]} functions
  * @return {Function}
  */
-const compose = (functions) => {
-  return function (x) {
-
+const compose = functions =>
+  function (input) {
+    let result = input
+    for (let i = functions.length - 1; i >= 0; i--) {
+      result = functions[i](result)
+    }
+    return result
   }
-}
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
