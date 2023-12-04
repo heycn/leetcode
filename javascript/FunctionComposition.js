537c13e4599d150913082ea09ea311253f750090
heycn <heycn@foxmail.com>
2023-12-04 16:47:53 +0800

    Add function composition implementation

---
+++ javascript/FunctionComposition.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+const compose = (functions) => {
+  return function (x) {
+
+  }
+}
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
